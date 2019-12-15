5c037f89b936906ad3f5088f5584de6e802216ba
AviNagosa <avinagossa91@gmail.com>
2019-12-15 04:22:20 +0200

    added four activitys, still unfinished

---
+++ app/src/main/java/com/example/fillmycart/ProductsActivity.java
@@ -0,0 +1,155 @@
+package com.example.fillmycart;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class ProductsActivity extends AppCompatActivity {
+
+    private TextView _fruit,_milk,_bread,_frozen,_meatAndFish,_sale;
+    private  TextView _Canning,_drinks,_candy,_Cleaners,_cosmeticsm,_shampo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_products);
+
+        _fruit = (TextView) findViewById(R.id.fruit_veg);
+        _milk = (TextView) findViewById(R.id.milk_eggs);
+        _bread = (TextView) findViewById(R.id.bread);
+        _frozen = (TextView) findViewById(R.id.frozen);
+        _meatAndFish = (TextView) findViewById(R.id.meat_chicken_fish);
+        _Canning = (TextView) findViewById(R.id.Canning);
+        _drinks = (TextView)findViewById(R.id.drinks);
+        _candy = (TextView) findViewById(R.id.candy);
+        _Cleaners = (TextView) findViewById(R.id.Cleaners);
+        _cosmeticsm = (TextView) findViewById(R.id.cosmetics);
+        _shampo = (TextView) findViewById(R.id.shampo);
+        _sale = (TextView)findViewById(R.id.sales);
+
+        _fruit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(ProductsActivity.this, AddProductActivity.class);
+                intent.putExtra("category", "ירקות ופירות");
+                startActivity(intent);
+            }
+        });
+
+        _milk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(ProductsActivity.this, AddProductActivity.class);
+                intent.putExtra("category", "מוצרי חלב וביצים");
+                startActivity(intent);
+            }
+        });
+
+        _bread.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(ProductsActivity.this, AddProductActivity.class);
+                intent.putExtra("category", "מאפיה ובית לחם");
+                startActivity(intent);
+            }
+        });
+
+        _frozen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(ProductsActivity.this, AddProductActivity.class);
+                intent.putExtra("category", "מזון קפוא");
+                startActivity(intent);
+            }
+        });
+
+        _meatAndFish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(ProductsActivity.this, AddProductActivity.class);
+                intent.putExtra("category", "בשר,עוף ודגים");
+                startActivity(intent);
+            }
+        });
+
+        _Canning.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(ProductsActivity.this, AddProductActivity.class);
+                intent.putExtra("category", "בישול,אפיה ושימורים");
+                startActivity(intent);
+            }
+        });
+
+        _drinks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(ProductsActivity.this, AddProductActivity.class);
+                intent.putExtra("category", "משקאות");
+                startActivity(intent);
+            }
+        });
+
+        _candy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(ProductsActivity.this, AddProductActivity.class);
+                intent.putExtra("category", "דגנים,חטיפים ומתוקים");
+                startActivity(intent);
+            }
+        });
+
+        _Cleaners.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(ProductsActivity.this, AddProductActivity.class);
+                intent.putExtra("category", "ניקיון");
+                startActivity(intent);
+            }
+        });
+
+        _cosmeticsm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(ProductsActivity.this, AddProductActivity.class);
+                intent.putExtra("category", "איפור ובישום");
+                startActivity(intent);
+            }
+        });
+
+        _shampo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(ProductsActivity.this, AddProductActivity.class);
+                intent.putExtra("category", "סבון,שמפו ומרכך");
+                startActivity(intent);
+            }
+        });
+
+        _sale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(ProductsActivity.this, AddProductActivity.class);
+                intent.putExtra("category", "מבצעים");
+                startActivity(intent);
+            }
+        });
+
+
+
+
+    }
+}
